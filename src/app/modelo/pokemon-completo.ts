9dd5c98cc47613c8ccbf1558f2d186ceb13f04b9
edo-nunez <ed.nunez@duocuc.cl>
2022-10-11 14:38:07 -0300

    Y sigue faltando

--- src/app/modelo/pokemon-completo.ts
+++ src/app/modelo/pokemon-completo.ts
@@ -34,6 +34,10 @@ type Sprites = {
   front_female? : string,
   front_shiny? : string,
   front_shiny_female? : string
+  back_default: string,
+  back_female?: string,
+  back_shiny?: string,
+  back_shiny_female?: string,
 }
 
 type Stat2 = {
